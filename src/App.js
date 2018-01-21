c346c3b793abf1133379fede9d7831992ea0ee39
Klaaz0r <klaas.foppen@gmail.com>
2018-01-21 22:09:34 +0100

    clean up code

--- src/App.js
+++ src/App.js
@@ -7,48 +7,6 @@ import GroupBuys from './groupbuys/GroupBuys'
 import Vendors from './vendors/Vendors'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.getInitialState = this.getInitialState.bind(this);
-    this.selectList = this.selectList.bind(this);
-    this.filterList = this.filterList.bind(this);
-
-    this.state = {
-      items: [],
-      filtered: []
-    };
-  }
-
-  filterList(event) {
-    const updatedList = this.state.items.filter(function(item) {
-      const keyword = event.target.value.toLowerCase()
-      const name = item.name.toLowerCase()
-      return name.includes(keyword)
-    });
-    this.setState({filtered: updatedList});
-  }
-
-  selectList(event) {
-    const updatedList = this.state.items.filter(function(item) {
-      const keyword = event.target.value.toLowerCase()
-      if (keyword === 'default') {
-        return true;
-      }
-      const name = item.tag.toLowerCase()
-      return name === keyword
-    });
-    this.setState({filtered: updatedList});
-  }
-
-  getInitialState() {
-    return fetch('/groupbuys.json').then((resp) => resp.json())
-  }
-
-  componentWillMount() {
-    this.getInitialState().then(items => this.setState({filtered: items, items: items}))
-  }
-
   render() {
     return (<div className="container">
       <Navbar/>
